af5851991f9fcc2220e789c30c08d6a31a6d5a88
Chris <cspollard@gmail.com>
2014-05-05 17:58:13 -0400

    testing new histogram definition.

--- Hist.hs
+++ Hist.hs
@@ -1,83 +1,56 @@
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE GADTs #-}
 
 module Hist where
 
-import Control.Arrow
-import Control.Applicative
-import qualified Data.Map as M
-import Data.Monoid
-import Data.Maybe (fromJust)
+import Data.Monoid ((<>))
 
 
-data Bin r = Range r r | Val r deriving (Eq, Show, Read)
+-- histograms are parameterized by the binning parameter (b) and the
+-- content parameter (v)
+class Hist h where
+    extractH :: h b v -> b -> v
+    fillH :: (Num v) => h b v -> b -> v -> h b v
 
-type ValUncert v = (v, v)
-
-instance Functor Bin where
-    fmap f (Range x y) = Range (f x) (f y)
-    fmap f (Val v) = Val (f v)
-
-
--- most important part: for filling histograms
-instance (Ord a) => Ord (Bin a) where
-    compare (Range w x) (Range y z) = let c = compare w y in
-                                        case c of
-                                            EQ -> compare x z
-                                            _ -> c
-
-    compare (Range x y) (Val z) = case (compare z x, compare z y) of
-                                    (GT, LT) -> EQ
-                                    (EQ, LT) -> EQ
-                                    (LT, _) -> LT
-                                    _ -> GT
-
-    compare v@(Val _) r@(Range _ _) = compare r v
-
-    compare (Val x) (Val y) = compare x y
+class Bin b where
+    containsB :: b a v -> a -> Bool
+    fillB :: (Num v) => b a v -> v -> b a v
 
 
-{-
-instance Applicative Bin where
-    pure = Val
+data Interval a = Ord a => Interval a a
 
-    Range f g <*> Range x y = Range (f x) (g y)
-    Val f <*> Val x = Val (f x)
+instance Eq a => Eq (Interval a) where
+    (Interval w x) == (Interval y z) = w == y && x == z
 
-    Range f g <*> Val x = Range (f x) (g x)
-    Val f <*> Range x y = Range (f x) (f y)
--}
+instance Ord a => Ord (Interval a) where
+    (Interval w x) `compare` (Interval y z) = w `compare` y <> x `compare` z
 
 
-type Hist r v = M.Map (Bin r) v
-type Hist1D v = M.Map (Bin Double) v
-type Hist2D v = M.Map (Bin Double) v
+data HistBin b v = HistBin {
+    bin :: Interval b,
+    val :: v
+    }
+
+instance Bin HistBin where
+    containsB (HistBin (Interval xmin xmax) _) x = xmin <= x && x < xmax
+
+    fillB (HistBin b w) wgt = HistBin b (w+wgt)
 
 
-
-binsFromList :: [a] -> [Bin a]
-binsFromList [] = []
-binsFromList [_] = []
-binsFromList (x:y:xs) = Range x y : binsFromList (y:xs)
-
-histWithDefaultContent :: Ord b => [b] -> v -> Hist b v
-histWithDefaultContent bins def = M.fromList $ zip (binsFromList bins) (repeat def)
-
-fillWeight :: (Ord b, Num v) => Hist b v -> b -> v -> Hist b v
-fillWeight h x w = M.adjust (w +) (Val x) h
+data Histogram b v = Histogram [HistBin b v]
 
 
-fill :: (Ord b, Num v) => Hist b v -> b -> Hist b v
-fill h x = M.adjust (1 +) (Val x) h
+fillBIfContains :: (Bin b, Num v) => [b a v] -> a -> v -> [b a v]
+fillBIfContains (b:bs) x wgt = if b `containsB` x
+                            then b `fillB` wgt : bs
+                            else b : fillBIfContains bs x wgt
+fillBIfContains [] _ _ = []
 
-midPoint :: (Fractional b) => Bin b -> b
-midPoint (Range x y) = (x + y)/2.0
-midPoint (Val x) = x
 
-histToPoints :: (Fractional b) => Hist b v -> [(b, v)]
-histToPoints h = map (first midPoint) $ M.toList h
+instance Hist Histogram where
+    fillH (Histogram bs) x wgt = Histogram $ fillBIfContains bs x wgt
 
-mapBins :: Ord c => (b -> c) -> Hist b v -> Hist c v
-mapBins f = M.mapKeys (fmap f)
+    extractH (Histogram (b:bs)) x = if b `containsB` x
+                                        then val b
+                                        else extractH (Histogram bs) x
 
-mapContents :: (v -> w) -> Hist b v -> Hist b w
-mapContents = M.map
+    extractH (Histogram []) _ = error "no bin containing value found in histogram."
