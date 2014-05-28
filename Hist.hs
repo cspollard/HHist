6c70965632a7d803ea66564201c91720b35341cb
Chris <cspollard@gmail.com>
2014-05-28 11:58:56 -0400

    added default hist filler.

--- Hist.hs
+++ Hist.hs
@@ -8,15 +8,15 @@ import Data.Monoid ((<>))
 -- histograms are parameterized by the binning parameter (b) and the
 -- content parameter (v)
 class Hist h where
-    extractH :: h b v -> b -> v
-    fillH :: (Num v) => h b v -> b -> v -> h b v
+    extractH :: Ord b => h b v -> b -> v
+    fillH :: (Ord b, Num v) => h b v -> b -> v -> h b v
 
 class Bin b where
-    containsB :: b a v -> a -> Bool
-    fillB :: (Num v) => b a v -> v -> b a v
+    containsB :: Ord a => b a v -> a -> Bool
+    fillB :: Num v => b a v -> v -> b a v
 
 
-data Interval a = Ord a => Interval a a
+data Interval a = Interval a a deriving Show
 
 instance Eq a => Eq (Interval a) where
     (Interval w x) == (Interval y z) = w == y && x == z
@@ -26,9 +26,9 @@ instance Ord a => Ord (Interval a) where
 
 
 data HistBin b v = HistBin {
-    bin :: Interval b,
-    val :: v
-    }
+    interval :: Interval b,
+    value :: v
+    } deriving (Show, Ord, Eq)
 
 instance Bin HistBin where
     containsB (HistBin (Interval xmin xmax) _) x = xmin <= x && x < xmax
@@ -38,8 +38,12 @@ instance Bin HistBin where
 
 data Histogram b v = Histogram [HistBin b v]
 
+fromListWithDefault :: Ord b => v -> [b] -> [HistBin b v]
+fromListWithDefault _ [] = []
+fromListWithDefault _ [_] = []
+fromListWithDefault def (x:x':xs) = HistBin (Interval x x') def : fromListWithDefault def (x':xs)
 
-fillBIfContains :: (Bin b, Num v) => [b a v] -> a -> v -> [b a v]
+fillBIfContains :: (Bin b, Ord a, Num v) => [b a v] -> a -> v -> [b a v]
 fillBIfContains (b:bs) x wgt = if b `containsB` x
                             then b `fillB` wgt : bs
                             else b : fillBIfContains bs x wgt
@@ -50,7 +54,7 @@ instance Hist Histogram where
     fillH (Histogram bs) x wgt = Histogram $ fillBIfContains bs x wgt
 
     extractH (Histogram (b:bs)) x = if b `containsB` x
-                                        then val b
+                                        then value b
                                         else extractH (Histogram bs) x
 
     extractH (Histogram []) _ = error "no bin containing value found in histogram."
